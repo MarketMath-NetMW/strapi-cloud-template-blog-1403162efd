a121f4986667442e376a5a7ef745e511238d83f7
janne86 <jan-erik.bredahl@marketmath.se>
2024-01-16 13:13:35 +0100

    Testing adding new collection types

---
+++ src/api/net-mw-test-news-post/routes/net-mw-test-news-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * net-mw-test-news-post router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::net-mw-test-news-post.net-mw-test-news-post');
---
+++ src/api/net-mw-test-news-post/services/net-mw-test-news-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * net-mw-test-news-post service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::net-mw-test-news-post.net-mw-test-news-post');
